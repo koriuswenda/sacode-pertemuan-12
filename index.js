a48dc3832afd4ed82f468ae0969feddfa0158823
korius kussa wenda <koriuswenda@gmail.com>
2024-07-02 16:48:05 +0700

    commit pertama

---
+++ index.js
@@ -0,0 +1,7 @@
+// Menambahakan Teks
+document.getElementById("demo").textContent = "Hello Word";
+
+// Method Menambahkan Teks Dom Tag
+const create = document.createElement("p");
+create.textContent = "Hello Korius ";
+document.body.appendChild(create);
